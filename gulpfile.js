5fd3092c6fc6a3f74900a92e72a304b505d133bf
Lucas Hill <lucas.s.hill@gmail.com>
2015-10-11 20:19:06 -0700

    More gulp automation

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,32 @@
-var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
-var babel = require('gulp-babel');
-var concat = require('gulp-concat');
-var nodemon = require('gulp-nodemon')
+var gulp = require('gulp'),
+sourcemaps = require('gulp-sourcemaps'),
+babel = require('gulp-babel'),
+concat = require('gulp-concat'),
+nodemon = require('gulp-nodemon');
+
+var JS_SOURCE_DIR = 'es6/**/*.es6';
+
+
+
+gulp.task('babel', function() {
+	return gulp.src(JS_SOURCE_DIR)
+	.pipe(sourcemaps.init())
+	.pipe(babel())
+	.pipe(concat('all.js'))
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', ['babel', 'start']);
 
 gulp.task('start', function () {
-  nodemon({
-    script: 'server.js', 
-    ext: 'js html', 
-    env: { 'NODE_ENV': 'development' }
-  })
-})
- 
-gulp.task('default', function () {
-    return gulp.src('es6/**/*.es6')
-        .pipe(sourcemaps.init())
-        .pipe(babel())
-        .pipe(concat('all.js'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist/js'));
+	nodemon({
+		script: 'server.js', 
+		ext: 'js html', 
+		env: { 'NODE_ENV': 'development' }
+	})
+});
+
+gulp.task('watch', function() {
+  gulp.watch(JS_SOURCE_DIR, ['babel']);
 });
